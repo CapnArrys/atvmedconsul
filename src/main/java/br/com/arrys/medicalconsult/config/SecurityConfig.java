54c30330822a48367f87b5500fd16bf799fcd521
Professor <professor@ACADEMICOS.COM>
2024-11-18 20:58:34 -0300

    cors fix

--- src/main/java/br/com/arrys/medicalconsult/config/SecurityConfig.java
+++ src/main/java/br/com/arrys/medicalconsult/config/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -76,6 +81,17 @@ public class SecurityConfig {
                 .withUser("Sicrano").password(passwordEncoder.encode("12345")).roles("PACIENTE");
 
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        return source;
     }
 
 }
