6a3485613fde7cb30ea170bf865e9680f2f192cc
Professor <professor@ACADEMICOS.COM>
2024-11-18 21:08:55 -0300

    cors fix 2

--- src/main/java/br/com/arrys/medicalconsult/config/SecurityConfig.java
+++ src/main/java/br/com/arrys/medicalconsult/config/SecurityConfig.java
@@ -91,6 +91,7 @@ public class SecurityConfig {
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/", configuration);
         return source;
     }
 
